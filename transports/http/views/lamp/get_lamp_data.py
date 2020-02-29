6cd72e70d1722f4b96d63418f78c21d872161788
Fertino <89114923376@yandex.ru>
2020-02-29 18:16:37 +0200

    переместил переменные

--- transports/http/views/lamp/get_lamp_data.py
+++ transports/http/views/lamp/get_lamp_data.py
@@ -8,10 +8,10 @@ from aiohttp import web
 
 
 async def get_lamp_data(request: aiohttp.web.Request) -> aiohttp.web.Response:
-    lumen: float = random.randint(0, 30)
-    light: float = random.randint(0, 100)
     turn_on: bool = True
     while True:
+        lumen: float = random.randint(0, 30)
+        light: float = random.randint(0, 100)
         lamp_data_result: Dict[str, Any] = dict(lumen=lumen, light=light, turn_on=turn_on)
         lamp_tmp: List[Dict] = []
         for i in range(0, 1000000):
