28f473225ddf83c393131d714e2b2ea774ad161d
Vladimir Tokarev <vt@appkode.ru>
2020-05-12 17:58:45 +0200

    Add routes

--- transports/__init__.py
+++
@@ -1,4 +0,0 @@
-from .routes import setup_routes
-
-
-__all__ = ['setup_routes']
--- transports/routes.py
+++
@@ -1,7 +0,0 @@
-from aiohttp import web
-
-from .http.routes import HTTP_ROUTES
-
-
-def setup_routes(app: web.Application) -> None:
-    app.add_routes(HTTP_ROUTES)
